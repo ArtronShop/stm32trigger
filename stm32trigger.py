7a054c1e05fa21b8546bd408850c5b142fe401ff
Max <7087907+maxpromer@users.noreply.github.com>
2024-10-19 21:37:02 +0700

    Fixed bug

--- stm32trigger.py
+++ stm32trigger.py
@@ -10,12 +10,13 @@ port = sys.argv[1]
 sys.stdout.write("==== STM32Trigger by ArtronShop CO.,LTD. ====\n")
 
 sys.stdout.write("Serial port {}\n".format(port))
-serialPort = serial.Serial(port, baudrate=115200, bytesize=8, timeout=0.5, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_EVEN)
+serialPort = serial.Serial(port, baudrate=115200, bytesize=8, timeout=0.1, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_EVEN)
 
 enter_bootloader_ok = False
 sys.stdout.write("Connect")
-for i in range(5):
-    # sys.stdout.write("-")
+for i in range(100000):
+    sys.stdout.write("-")
+    sys.stdout.flush()
     
     # Enter to Bootloader
     serialPort.rts = 1
@@ -31,28 +32,32 @@ for i in range(5):
     for i in range(5):
         sleep(0.3)
         sys.stdout.write(".")
+        sys.stdout.flush()
 
         # Clear old data
         serialPort.reset_input_buffer()
 
         # USARTx selected
         serialPort.write(bytes([ 0x7F ]))
-        got = serialPort.read(1)
-        if len(got) > 0 :
-            if got[0] == 0x79:
-                # sys.stdout.write("Get ACK\n")
-                sys.stdout.write(" Connected\n")
-                enter_bootloader_ok = True
-            # elif got[0] == 0x1F:
-            #     sys.stdout.write("Get NACK\n")
-        else:
-            continue
+        sleep(0.1)
+        while serialPort.in_waiting:
+            got = serialPort.read(1)
+            if len(got) > 0 :
+                if got[0] == 0x79:
+                    # sys.stdout.write("Get ACK\n")
+                    sys.stdout.write(" Connected\n")
+                    sys.stdout.flush()
+                    enter_bootloader_ok = True
+                # elif got[0] == 0x1F:
+                #     sys.stdout.write("Get NACK\n")
+            else:
+                continue
 
         # Clear old data
         serialPort.reset_input_buffer()
-
-        # Get Chip ID
         """
+        # Get Chip ID
+        
         serialPort.write(bytes([ 0x02, 0xFD ]))
         got = serialPort.read(1)
         if len(got) > 0 :
@@ -67,7 +72,7 @@ for i in range(5):
             elif got[0] == 0x1F:
                 sys.stdout.write("Get NACK\n")
         """
-
+        
         if enter_bootloader_ok:
             break
     if enter_bootloader_ok:
